b36e560b6ad4644dd60a6859184af42205b2c4bc
alexandru nazare <alexandrunazare@yahoo.it>
2025-03-20 00:51:57 +0100

    csr implementation completed

--- tests/open_matrici/apriMatrice.c
+++ tests/open_matrici/apriMatrice.c
@@ -18,9 +18,25 @@ int main(int argc, char *argv[])
     }
     fprintf(stdout, "nz=%d height=%d width=%dn", mat->nz, mat->height, mat->width);
     for(int i=0;i<mat->nz;i++){
-        fprintf(stdout, "%d %d %20.19g\n", mat->iVettore[i]+1, mat->jVettore[i]+1, mat->valori[i]);
+       fprintf(stdout, "%d %d %20.19g\n", mat->iVettore[i], mat->jVettore[i], mat->valori[i]);
     }
-
+    struct MatriceCsr *csrMatrice;
+    convertRawToCsr(mat,&csrMatrice);
+    printf("[ ");
+    for(int i=0;i<=csrMatrice->width;i++){
+        printf("%d ",csrMatrice->iRP[i]);
+    }
+    printf("]\n");
+    printf("[ ");
+    for(int i=0;i<csrMatrice->nz;i++){
+        printf("%d ",csrMatrice->jValori[i]);
+    }
+    printf("]\n");
+    printf("[ ");
+    for(int i=0;i<csrMatrice->nz;i++){
+        printf("%f ",csrMatrice->valori[i]);
+    }
+    printf("]\n");
 	return 0;
 }
 
