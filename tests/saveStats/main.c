6a3acca3743c6b4477fc69b9136be11366739922
alexandru nazare <alexandrunazare@yahoo.it>
2025-04-15 12:09:11 +0200

    Revert "xcxzc"
    This reverts commit 1eeec76abfbc37bcde79cf5bbd08dc4745113c49.

--- tests/saveStats/main.c
+++ tests/saveStats/main.c
@@ -307,7 +307,7 @@ if (flatHll != 0){
 }
 
 //------------------------------CUDA HLL  1-----------------------------//
-for(unsigned int  j=64;j<200;j=j*2){
+for(unsigned int  j=32;j<200;j=j*2){
     struct CsvEntry result;
     struct Vector *resultV;
     generateEmpty(rows, &resultV);
@@ -328,14 +328,13 @@ for(unsigned int  j=64;j<200;j=j*2){
         double diff;
         double percentage;
         calculate_vector_differences(resultV,resultSerial,&diff,&percentage);
-        result.errorPercentage=percentage;
-        result.errorValue=diff;
+        
         append_csv_entry(csv,&result);
     }
     freeRandom(&resultV); 
 }
 //------------------------------CUDA HLL  2-----------------------------//
-for(unsigned int  j=64;j<200;j=j*2){
+for(unsigned int  j=32;j<200;j=j*2){
     struct CsvEntry result;
     struct Vector *resultV;
     generateEmpty(rows, &resultV);
@@ -356,14 +355,13 @@ for(unsigned int  j=64;j<200;j=j*2){
         double diff;
         double percentage;
         calculate_vector_differences(resultV,resultSerial,&diff,&percentage);
-        result.errorPercentage=percentage;
-        result.errorValue=diff;
+        
         append_csv_entry(csv,&result);
     }
     freeRandom(&resultV); 
 }
 //------------------------------CUDA HLL  3-----------------------------//
-for(unsigned int  j=64;j<200;j=j*2){
+for(unsigned int  j=32;j<200;j=j*2){
     struct CsvEntry result;
     struct Vector *resultV;
     generateEmpty(rows, &resultV);
@@ -384,8 +382,7 @@ for(unsigned int  j=64;j<200;j=j*2){
         double diff;
         double percentage;
         calculate_vector_differences(resultV,resultSerial,&diff,&percentage);
-        result.errorPercentage=percentage;
-        result.errorValue=diff;
+        
         append_csv_entry(csv,&result);
     }
     freeRandom(&resultV); 
